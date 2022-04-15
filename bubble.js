5e8db3f172c968bc413492aee36ffffe1f359064
stevtang <stevenmtang1@gmail.com>
2022-04-15 11:53:17 -0700

    done with bubble

--- bubble.js
+++ bubble.js
@@ -1,3 +1,12 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+  }
+  return nums;
+}
 
 module.exports = bubbleSort;
