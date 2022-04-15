bb9f25cbfc3273f1412f2981afd2b77a9c8e08cd
Lidan-Huang <huangld0314@gmail.com>
2022-04-15 12:15:40 -0700

    finished merge function.

--- merge.js
+++ merge.js
@@ -1,4 +1,28 @@
-function merge() {}
+"use strict";
+
+function merge(sorted1, sorted2) {
+  let merged = [];
+  let i = 0;
+  let j = 0;
+  while((i < sorted1.length) && (j < sorted2.length)) {
+    if (sorted1[i] <= sorted2[j]) {
+      merged.push(sorted1[i]); 
+      i++;
+    } else {
+      merged.push(sorted2[j]);
+      j++;
+    }
+  }
+
+  if (i !== sorted1.length - 1) {
+    merged.push(...sorted1.slice(i));
+  } 
+  if (j !== sorted2.length - 1){
+    merged.push(...sorted2.slice(j));
+  }
+
+  return merged;
+}
 
 function mergeSort() {}
 
